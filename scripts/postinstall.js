05a6e09cfb6ebf35f7c56e0a5130984eb87c8046
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2019-03-14 16:21:38 +0100

    use chalk in postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const packageJson = require('../package.json');
 
-const RESET = '\x1b[0m';
-const RED   = '\x1b[31m';
-const GREEN = '\x1b[32m';
-const BLUE  = '\x1b[34m';
-
-console.info(`
-${GREEN}Successfully installed ${RED}react-native-unimodules${GREEN}. This package contains core unimodules that are commonly depended on by other unimodules. You will need to configure your project before using other unimodules like ${RED}expo-camera${GREEN}, ${RED}expo-media-library${GREEN}.
+console.info(chalk.green(`
+Successfully installed ${chalk.red(packageJson.name)}. This package contains core unimodules that are commonly depended on by other unimodules. You will need to configure your project before using other unimodules like ${chalk.red('expo-camera')}, ${chalk.red('expo-media-library')} and others.
 See configuration guide:
-  ${BLUE}https://www.npmjs.com/package/react-native-unimodules/v/${packageJson.version}${RESET}
-`);
+  ${chalk.blue(`https://www.npmjs.com/package/${packageJson.name}/v/${packageJson.version}`)}
+`));
